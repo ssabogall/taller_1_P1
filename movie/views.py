43d5ca7fed6dffe8234e8cb1792b2343aedeca5c
ssabogall@eafit.edu.co <tu-email@ejemplo.com>
2025-07-31 11:21:51 -0500

    cambios en el views

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home (request):
-    return render(request, 'home.html', { 'name': 'Greg Lim' })
+    return render(request, 'home.html', { 'name': 'Santiago Sabogal Lozano' })
 
 def about (request):
     return HttpResponse("About Page")
